ff2177a70600eb5be0397e1999d1464b775423c3
Nguyễn Chí Thành Lộc <96804285+GinzaTech@users.noreply.github.com>
2022-02-28 13:49:33 +0700

    Update zwar.js

--- zwar.js
+++ zwar.js
@@ -2,10 +2,10 @@ module.exports.config = {
     name: "zwar",
     version: "1.0.4",
     credits: "GinzaTech & Michael",
-    description: "Tham gia chiến trường zombie trên chính box chat của bạn",
-    usages: "zwar [register/shop/upgrade/info/prison/status/sell]",
-    commandCategory: "game",
-    cooldowns: 0,
+    description: "Chiến đấu với zombie",
+    usages: "[register/shop/upgrade/info/prison/status/sell]",
+    commandCategory: "Zombie War",
+    cooldowns: 5,
     dependencies: {
          "fs-extra": "",
          "axios": ""
@@ -56,7 +56,7 @@ module.exports.handleReaction = async ({ api, event, handleReaction, Currencies 
                 var data = userData;
                 data.zwar = zwar;
                 await Currencies.setData(event.userID, { money, data });
-                return api.sendMessage(`[Shop Hệ Thống Vũ Khí] Bạn đã mua thành công ${handleReaction.choose} vị trí!`, event.threadID, event.messageID);
+                return api.sendMessage(`[ SHOP ] » Bạn đã mua thành công ${handleReaction.choose} vị trí!`, event.threadID, event.messageID);
             }
             default:
                 break;
@@ -64,7 +64,7 @@ module.exports.handleReaction = async ({ api, event, handleReaction, Currencies 
     }
     catch (e) {
         console.log(e);
-        return api.sendMessage("[Zombie War] zombie đã tiêu diệt hết người chơi", event.threadID, event.messageID);
+        return api.sendMessage("[ ZWar ] zombie đã tiêu diệt hết người chơi", event.threadID, event.messageID);
     }
 }
 
@@ -90,10 +90,9 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
                         i++;
                     }
                     return api.sendMessage(
-                        "[ ❖-❖-❖ ] [ = [ Shop Weapon] = ] [ ❖-❖-❖ ]" +
-                        "\n[ ❖-❖ ] Hãy Lựa Chọn Theo Cách Của Bạn [ ❖-❖ ]\n\n" +
+                        "==== [ SHOP WEAPON ] ====" +
                         entryList.join("\n") +
-                        "\n[ ❖ ] => Hãy Trả Lời Tin Nhắn Bot Và Chọn Theo Số Thứ Tự <= [ ❖ ]"
+                        "\nReply tin nhắn này với vũ khí bạn muốn mua"
                     , event.threadID, (error, info) => {
                         global.client.handleReply.push({
                             name: this.config.name,
@@ -109,18 +108,18 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
                         index = 0,
                         zwar = userData.data.zwar;
 
-                    for (gun of zwar.critters) {
+                    for (let gun of zwar.critters) {
                         moneyAll += gun.price;
                         zwar.critters[index] = emptygun;
                         index++;
                     }
                     const money = userData["money"] += moneyAll;
                     await Currencies.setData(event.senderID,{ money, zwar });
-                    return api.sendMessage(`[ Zombie War ] Tổng số tiền bạn bán được là: ${moneyAll} coins`, event.threadID, event.messageID);
+                    return api.sendMessage(`[ ZWar ] » Tổng số tiền bạn bán được là: ${moneyAll}$`, event.threadID, event.messageID);
                 }
                 case "3": {
                     const userData = (await Currencies.getData(event.senderID)).data.zwar;
-                    return api.sendMessage(`[ = ] Shop Weapon (Upgrade) [ = ]\n\n[!] Hiện tại bạn đang có ${userData.critters.length += 1} vị trí có thể chứa đồ trong kho đồ của bạn\n[ ❖ ] => Để Nâng Cấp Hãy Reply Tin Nhắn Và Ghi Số Slot <= [❖]`, event.threadID, (error, info) => {
+                    return api.sendMessage(`[ = ] UPGRADE BALO [ = ]\nHiện tại bạn đang có ${userData.critters.length += 1} vị trí có thể chứa đồ trong kho đồ của bạn\n\nReply tin nhắn này cùng số slot bạn muốn nâng cấp`, event.threadID, (error, info) => {
                         global.client.handleReply.push({
                             name: this.config.name,
                             messageID: info.messageID,
@@ -139,24 +138,22 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
             try {
                 const choose = parseInt(event.body);
                 var userData = (await Currencies.getData(event.senderID));
-                if (isNaN(event.body)) return api.sendMessage("[Zombie War] Lựa chọn của bạn không phải là một con số!", event.threadID, event.messageID);
-                if (choose > datagun.length || choose < datagun.length) return api.sendMessage("[Zombie War] Lựa chọn của bạn vượt quá danh sách", event.threadID, event.messageID);
+                if (isNaN(event.body)) return api.sendMessage("[ ERROR ] » Lựa chọn của bạn không phải là một con số!", event.threadID, event.messageID);
+                if (choose > datagun.length || choose < datagun.length) return api.sendMessage("[ ERROR ] » Lựa chọn của bạn vượt quá danh sách", event.threadID, event.messageID);
                 const gunUserChoose = datagun.gun[choose - 1];
-                if (userData.money < gunUserChoose.price) return api.sendMessage("[Zombie War] Bạn không đủ tiền để có thể súng mới", event.threadID, event.messageID);
+                if (userData.money < gunUserChoose.price) return api.sendMessage("[ ERROR ] » Bạn không đủ tiền để có thể súng mới", event.threadID, event.messageID);
                 userData.data.zwar.weapon.name = gunUserChoose.name;
                 userData.data.zwar.weapon.price = gunUserChoose.price;
                 userData.data.zwar.weapon.time = gunUserChoose.time;
-                console.log(userData['data']['zwar']['weapon']);
                 userData.money = userData.money - gunUserChoose.price;
                 var data = userData;
                 data.zwar = userData.data.zwar;
                 await Currencies.setData(event.senderID, { money: userData.money, data });
-                console.log((await Currencies.getData(event.senderID)).data.zwar);
-                return api.sendMessage(`[Shop weapon] Bạn đã mua thành công: ${gunUserChoose.name} với giá ${gunUserChoose.price} coins!`, event.threadID, event.messageID);
+                return api.sendMessage(`[ SHOP ] » Bạn đã mua thành công ${gunUserChoose.name} với giá ${gunUserChoose.price}$`, event.threadID, event.messageID);
             }
             catch (e) {
                 console.log(e);
-                return api.sendMessage("[Zombie War] Hiện tại không thể mở shop vì đã xảy ra lỗi hệ thống, vui lòng thử lại sau hoặc đợi thông báo!", event.threadID, event.messageID); 
+                return api.sendMessage("Đã xảy ra lỗi!", event.threadID, event.messageID); 
             }
         }
         //upgrade
@@ -164,11 +161,10 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
             try {
                 const choose = parseInt(event.body);
                 var userData = (await Currencies.getData(event.senderID));
-                if (isNaN(event.body)) return api.sendMessage("[Zombie War] Lựa chọn của bạn không phải là một con số!", event.threadID, event.messageID);
-                if (choose <= 0) return api.sendMessage("[Zombie War] Lựa chọn của bạn không phải là số âm!", event.threadID, event.messageID);
+                if (isNaN(event.body)) return api.sendMessage("[ ERROR ] » Lựa chọn của bạn không phải là một con số!", event.threadID, event.messageID);
                 const moneyOfUpgrade = choose * 2000;
-                if (userData.money < moneyOfUpgrade) return api.sendMessage(`[Shop Weapon] Bạn không đủ tiền để có thể mua thêm chỗ cho túi đồ, bạn còn thiếu khoảng ${moneyOfUpgrade - userData.money}`, event.threadID, event.messageID);
-                return api.sendMessage(`[Shop Weapon] Bạn đang cần mua ${choose} vị trí với giá ${moneyOfUpgrade} coins, nếu bạn đồng ý có thể reaction tin này!`, event.threadID, (error, info) => {
+                if (userData.money < moneyOfUpgrade) return api.sendMessage(`[ SHOP ] » Bạn không đủ tiền để có thể mua thêm chỗ cho túi đồ, bạn còn thiếu ${moneyOfUpgrade - userData.money}$`, event.threadID, event.messageID);
+                return api.sendMessage(`[ SHOP ] » Bạn muốn mua ${choose} với giá ${moneyOfUpgrade} không? \n\nReaction tin nhắn này để đồng ý!`, event.threadID, (error, info) => {
                     global.client.handleReaction.push({
                         name: this.config.name,
                         messageID: info.messageID,
@@ -180,7 +176,7 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
             }
             catch (e) {
                 console.log(e);
-                return api.sendMessage("[Zombie War] Hiện tại không thể tham gia vì đã xảy ra lỗi hệ thống, vui lòng thử lại sau hoặc đợi thông báo!, Code : 001", event.threadID, event.messageID);
+                return api.sendMessage("Đã xảy ra lỗi!", event.threadID, event.messageID);
             }
         }
         default: {
@@ -191,7 +187,7 @@ module.exports.handleReply = async function({ api, event, client, handleReply, C
 
 module.exports.makeEmptyCritterList = () => {
     var zombieList = [];
-    for (i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         zombieList.push({
             name: "Empty",
             size: 0.0,
@@ -202,7 +198,7 @@ module.exports.makeEmptyCritterList = () => {
 }
 
 module.exports.getRarity = () => { 
-    this.getRarityRecursion(Math.floor(Math.random() * Math.floor(100)), -1, 0)
+    return this.getRarityRecursion(Math.floor(Math.random() * Math.floor(100)), -1, 0);
 }
 
 module.exports.getRarityRecursion = (chance, index, number) => {
@@ -233,15 +229,15 @@ module.exports.getRarityRecursion = (chance, index, number) => {
 
 module.exports.getZombie = (zombieRarity, currentHour, currentMonth) => {
     const { readFileSync } = require ("fs-extra");
-    var dataZombie = require('./cache/zwar/data.json');
-    var newZombieData = dataZombie.Zombie.filter(Zombie => Zombie.time.includes(currentHour) && Zombie.months.includes(currentMonth) && Zombie.rarity.includes(zombieRarity));
+    var { Zombie } = require('./cache/zwar/data.json');
+    var newZombieData = Zombie.filter(z => (z.time.indexOf(parseInt(currentHour)) !== -1) && (z.months.indexOf(parseInt(currentMonth)) !== -1) && z.rarity == zombieRarity);
     return newZombieData;
 }
 
 module.exports.addCritter = (user, critter, api, event) => {
     if (user.critters[user.critters.length - 1].price != 0 || typeof user.critters[user.critters.length - 1].price == "undefined") api.sendMessage("[zwar] Túi của bạn không còn đủ không gian lưu trữ!", event.threadID, event.messageID);
     else {
-        for (i = 0; i < user.critters.length; i++) {
+        for (let i = 0; i < user.critters.length; i++) {
             if (user.critters[i].price === 0) {
                 user.critters[i] = critter;
                 i = user.critters.length;
@@ -259,9 +255,10 @@ module.exports.run = async function({ api, event, args, client, Currencies, User
     };
     var dataUser = (await Currencies.getData(event.senderID)).data.zwar || {};
     switch (args[0]) {
-        case "register": {
+        case "register": 
+        case "-r": {
             try {
-                if (Object.entries(dataUser).length != 0) return api.sendMessage("[Zombie War] Bạn đã từng đăng ký vào chiến trường!", event.threadID, event.messageID);
+                if (Object.entries(dataUser).length != 0) return api.sendMessage("[ ZWar ] » Bạn đã có mặt trên chiến trường!", event.threadID, event.messageID);
                 var s = {};
                 s['zwar'] = {};
                 s['zwar'].weapon = emptygun;
@@ -269,22 +266,22 @@ module.exports.run = async function({ api, event, args, client, Currencies, User
                 var data = (await Currencies.getData(event.senderID));
                 data = s;
                 await Currencies.setData(event.senderID, { data });
-                return api.sendMessage("[Zombie War] Bạn đã đăng ký vào chiến trường thành công", event.threadID, event.messageID);
+                return api.sendMessage("[ ZWar ] » Bạn đã đăng ký vào chiến trường thành công!", event.threadID, event.messageID);
             }
             catch (e) {
                 console.log(e);
-                return api.sendMessage("[Zombie War] Hiện tại không thể đăng ký vì đã xảy ra lỗi hệ thống, vui lòng thử lại sau hoặc đợi thông báo!", event.threadID, event.messageID);
+                return api.sendMessage("Đã xảy ra lỗi!", event.threadID, event.messageID);
             }
         }
-        case "shop": {
-            if (Object.entries(dataUser).length == 0)return api.sendMessage("[Zombie War] Bạn cần đăng ký vào chiến trường, hãy sử dụng '[ /Zwar register ]'", event.threadID, event.messageID);
+        case "shop": 
+        case '-s': {
+            if (Object.entries(dataUser).length == 0)return api.sendMessage("[ ZWar ] » Bạn chưa có mặt trên chiến trường", event.threadID, event.messageID);
             return api.sendMessage(
-                "[ ❖ ] [ = [ Shop Weapon ] = ] [ ❖ ]" +
-                "\n[ ❖ ] Hãy Nhập Lựa Chọn [ ❖ ]" +
-                "\n[ ❖ ] Mua Súng [ 1 ] [ ❖ ]" +
-                "\n[ ❖ ] Bán Zombie [ 2 ] [ ❖ ]" +
-                "\n[ ❖ ] Nâng Cấp Kho [ 3 ] [ ❖ ]" +
-                "\n\n[ ❖ ] => Hãy Trả Lời Tin Nhắn Để Chọn !"
+                "==== [ Shop Weapon ] ====" +
+                "\n[1] Mua Súng" +
+                "\n[2] Bán Zombie" +
+                "\n[3] Nâng Cấp Kho" +
+                "\n\nReply tin nhắn này và đưa ra lựa chọn của bạn"
             , event.threadID, (error, info) => {
                 global.client.handleReply.push({
                     name: this.config.name,
@@ -294,8 +291,9 @@ module.exports.run = async function({ api, event, args, client, Currencies, User
                 });
             }, event.messageID);
         }
-        case "prison": {
-            if (Object.entries(dataUser).length == 0)return api.sendMessage("[Zombie War] Bạn cần đăng ký vào chiến trường, hãy sử dụng '[ /zwar register ]'", event.threadID, event.messageID);
+        case "prison": 
+        case '-p': {
+            if (Object.entries(dataUser).length == 0)return api.sendMessage("[ ZWar ] » Bạn chưa có mặt trên chiến trường", event.threadID, event.messageID);
             var listCritters = [],
                 msg = "",
                 index = 1;
@@ -331,45 +329,40 @@ module.exports.run = async function({ api, event, args, client, Currencies, User
         default: {
             try {
                 const format = new Intl.NumberFormat();
-                if (Object.entries(dataUser).length == 0)return api.sendMessage("[Zombie War] Bạn cần đăng ký vào chiến trường, hãy sử dụng '/zwar register'", event.threadID, event.messageID);
+                if (Object.entries(dataUser).length == 0)return api.sendMessage("[ ZWar ] » Bạn chưa có mặt trên chiến trường", event.threadID, event.messageID);
                 var dates = Math.floor((Math.abs(dataUser.time - new Date()) / 1000) / 60);
-                if (dataUser.weapon.price === 0) return api.sendMessage("[Zombie War] Bạn cần mua súng, hãy sử dụng 'zwar shop' để mua mới!", event.threadID, event.messageID);
-                else if (dates < dataUser.weapon.time) return api.sendMessage("[Zombie War] Bạn đang trong thời gian cooldown, hãy thử lại sau!", event.threadID, event.messageID);
+                if (dataUser.weapon.price === 0) return api.sendMessage("[ ZWar ] » Bạn chưa có súng!", event.threadID, event.messageID);
+                else if (dates < dataUser.weapon.time) return api.sendMessage("[ ZWar ] » Bạn đang trong thời gian chờ, hãy thử lại sau!", event.threadID, event.messageID);
                 else if (dataUser.weapon.duribility < 1) {
                     dataUser.weapon = emptygun;
-                    return api.sendMessage("[Zombie War] Súng của bạn đã hỏng, sử dụng '/zwar' để mua súng mới!", event.threadID, event.messageID);
+                    return api.sendMessage("[ ZWar ] » Súng của bạn đã hỏng, hãy mua súng mới!", event.threadID, event.messageID);
                 }
 
                 var zombieRarity = this.getRarity();
                 var currentHour = new Date().getHours();
                 var currentMonth = new Date().getMonth();
                 const zombieData = await this.getZombie(zombieRarity, currentHour, currentMonth);
-                if (!zombieData || zombieData.length == 0) return api.sendMessage("[Zombie War] Hiện tại không có zombie để bắn", event.threadID, event.messageID);
-
-                var today = new Date().toLocaleString("vi-vn", { timeZone: "Asia/Ho_Chi_Minh" });
+                if (!zombieData || zombieData.length == 0) return api.sendMessage("[ ZWar ] » Hiện tại không có zombie để bắn", event.threadID, event.messageID);
                 var caught = zombieData[Math.floor(Math.random() * ((zombieData.length - 1) - 0 + 1)) + 0];
                 caught.size = ((Math.random() * (caught.size[0] - caught.size[1]) + caught.size[1]).toFixed(1));
                 dataUser.critters = this.addCritter(dataUser, caught, api, event);
                 dataUser.weapon.duribility--;
                 await Currencies.setData(event.senderID, {zwar: dataUser});
-                const nameUser = (await Users.getData(event.senderID)).name || (await Users.getInfo(envent.senderID)).name;
+                const nameUser = (await Users.getData(event.senderID)).name || (await Users.getInfo(event.senderID)).name;
                
                 return api.sendMessage(
-                    "[ ❖-❖-❖ ] Bạn Đã Giết : [" + caught.name + "] [ ❖-❖-❖ ]" +
-                    "\n  [ ❖-❖ ] [ = ] [ = [ Thông Tin Chung ] = ] [ = ] [ ❖-❖ ]"+
-                    "\n[ ❖ ] [ Người bắt ] : {name} [ ❖ ] " 
+                    "[ ZWar ] » Bạn đã giết " + caught.name +
+                    "\n\n===== [ Thông Tin Chung ] ====="+
+                    `\n👤 Người bắt: ${nameUser}` 
                     .replace(/\{name}/g, nameUser)
-                +   "\n[ ❖ ] [ Kích Cỡ Zombie] : " + caught.size + " m" +
-                    "\n[ ❖ ] [ Độ Hiếm Zombie] : " + caught.rarity +
-                    "\n[ ❖ ] [ Mô Tả Zombie ] : " + caught.catch +
-                    "\n[ ❖ ] [ Xuất Hiện Vào Tháng ] : " + caught.months + 
-                    "\n[ ❖ ] [ Tổng Số Tiền Có Thể Kiếm Khi săn Được] : " + format.format(caught.price) + " Đô" +
-                    "\n[ ❖ ] " + today + "[ ❖ ]"
-                , event.threadID, event.messageID);
+                +   "\n✨ Kích cỡ: " + caught.size + "m" +
+                    "\n🧟‍♂️ Độ Hiếm Zombie: " + caught.rarity +
+                    "\n💬 Mô Tả: " + caught.catch + 
+                    "\n💰 Giá trị: " + format.format(caught.price) + "$", event.threadID, event.messageID);
             }
             catch (e) {
                 console.log(e);
-                return api.sendMessage("[Zombie War] Hiện tại không thể tham gia vì đã xảy ra lỗi hệ thống, vui lòng thử lại sau hoặc đợi thông báo!", event.threadID, event.messageID);
+                return api.sendMessage("Đã xảy ra lỗi!", event.threadID, event.messageID);
             }
         }
     }
